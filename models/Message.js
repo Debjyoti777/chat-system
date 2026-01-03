3625d5926272a07aef0ed624d5223e36ec9f8b2b
Debjyoti07 <debjyoti.sarkar.cse.2022@tint.edu.in>
2026-01-04 00:55:03 +0530

    Create Message.js

---
+++ models/Message.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: String,
+    status: {
+      type: String,
+      enum: ["sent", "delivered", "seen"],
+      default: "sent",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
